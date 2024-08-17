a94bdcc15f48cb5ea2b776868b62edd66560a1da
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-08-17 13:48:12 +0530

    controll_initializer.dart is created

---
+++ lib/controll_initializer.dart
@@ -0,0 +1,25 @@
+
+import 'package:hotel_manager_app/controllers/data/employee_data_controller.dart';
+import 'package:get/get.dart';
+import 'package:hotel_manager_app/controllers/network/employee_data_network_controller.dart';
+import 'package:hotel_manager_app/controllers/views/employee_screen/employee_list_builder.dart';
+
+class ControllerInitializer {
+  static bool _isIntialized = false;
+
+  static bool get isInitialized => _isIntialized;
+
+  static Future<void> initAllControllers() async {
+    try {
+
+      await Get.putAsync(() => EmployeeDataNetworkController.create());
+      Get.put(EmployeeDataController());
+      Get.put(EmployeeListBuilder());
+
+      _isIntialized = true;
+    } catch (e) {
+      print('An Unexpected error occurs initiating controller.');
+      rethrow;
+    }
+  }
+}

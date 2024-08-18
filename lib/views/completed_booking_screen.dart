db00c50a671fbc9f9f6cf8db0a05a83aed194d0c
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-08-18 20:33:59 +0530

    completed_booking_screen.dart is created, not finished.

---
+++ lib/views/completed_booking_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class CompletedBookingScreen extends StatelessWidget {
+  const CompletedBookingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

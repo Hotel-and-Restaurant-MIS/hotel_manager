93349638788409cb7fa7b280ccdc20fef37e997b
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-08-19 14:31:34 +0530

    TextInputType add as the parameter of the TextField.

--- lib/components/input_text_field.dart
+++ lib/components/input_text_field.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hotel_manager_app/constants/sub_title_text_style.dart';
 import 'package:hotel_manager_app/controllers/views/add_employee_screen/add_emp_state_controller.dart';
@@ -7,11 +8,13 @@ class InputTextField extends StatelessWidget {
   final String title;
   final String place_holder;
   final Function(String) submit_controller;
+  final TextInputType keyBoardType;
 
   InputTextField({
     required this.title,
     required this.place_holder,
     required this.submit_controller,
+    this.keyBoardType=TextInputType.name
   });
 
   @override
@@ -30,9 +33,10 @@ class InputTextField extends StatelessWidget {
                 style: TextConstants.kMainTextStyle(),
               ),
               TextField(
+                keyboardType: keyBoardType,
                 decoration: InputDecoration(
                   hintText: place_holder,
-                  hintStyle: TextConstants.kSubTextStyle(),
+                  hintStyle: TextConstants.kSubTextStyle(fontSize: 17.0),
                 ),
                 controller: _textFieldController,
                 onSubmitted: submit_controller,

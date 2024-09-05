0f0f841311e9c455fa82137e13344c76d70a375d
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-05 10:16:43 +0530

    add form validations to the room booking

--- lib/controllers/views/create_booking_screen/create_booking_state_controller.dart
+++ lib/controllers/views/create_booking_screen/create_booking_state_controller.dart
@@ -4,11 +4,18 @@ import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:hotel_manager_app/controllers/data/booking_data_controller.dart';
 import 'package:hotel_manager_app/controllers/data/create_booking_data_controller.dart';
 import 'package:hotel_manager_app/models/booking.dart';
+import 'package:hotel_manager_app/models/form_valid_response.dart';
 
 class CreateBookingStateController extends GetxController {
   static CreateBookingStateController instance = Get.find();
 
   CreateBookingDataController _cbdc = CreateBookingDataController.instance;
+
+  final RegExp _nameRegExp = RegExp(r"^[a-zA-Z\s'-]+$");
+  final RegExp _phoneRegExp = RegExp(r"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$");
+  final RegExp _emailRegExp = RegExp(r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+  final RegExp _nicRegExp = RegExp(r"^\d{9}[Vv]|\d{12}$");
+  final RegExp _noOfRoomsRegExp = RegExp(r"^[1-9]\d*$");
 
   String? _name;
   String? _email;
@@ -87,5 +94,57 @@ class CreateBookingStateController extends GetxController {
     _cbdc.getRoomList(roomType: roomType, days: days);
 
     //}
+  }
+
+  FormValidResponse validationForm() {
+    final validationRules = [
+      {
+        'condition': _name == null || _name == '',
+        'message': 'Name cannot be empty!'
+      },
+      {
+        'condition': !_nameRegExp.hasMatch(_name ?? ''),
+        'message': 'Name is not valid!'
+      },
+      {
+        'condition': roomType == null,
+        'message': 'Select room Type!',
+      },
+      {
+        'condition': _email == null || _email == '',
+        'message': 'Email cannot be empty!'
+      },
+      {
+        'condition': !_emailRegExp.hasMatch(_email ?? ''),
+        'message': 'Email is not valid!'
+      },
+      {
+        'condition': _phoneNumber == null || _phoneNumber == '',
+        'message': 'Phone number cannot be empty!'
+      },
+      {
+        'condition': !_phoneRegExp.hasMatch(_phoneNumber ?? ''),
+        'message': 'Phone number is not valid!'
+      },
+      {
+        'condition': _nic == null || _nic == '',
+        'message': 'NIC cannot be empty!'
+      },
+      {
+        'condition': !_nicRegExp.hasMatch(_nic ?? ''),
+        'message': 'NIC is not valid!',
+      },
+      {
+        'condition':_noOfRooms == null,
+        'message':'No of rooms cannot be empty!'
+      }
+    ];
+    for (final rule in validationRules) {
+      if (rule['condition'] as bool) {
+        return FormValidResponse(
+            formValid: false, message: rule['message'] as String);
+      }
+    }
+    return FormValidResponse(formValid: true);
   }
 }

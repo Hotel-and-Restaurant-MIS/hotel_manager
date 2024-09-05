4ba65c34dbc42d6750b519331c8d409e09b70bc5
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-05 10:20:14 +0530

    connected with backend

--- lib/controllers/network/employee_data_network_controller.dart
+++ lib/controllers/network/employee_data_network_controller.dart
@@ -1,21 +1,56 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:hotel_manager_app/constants/network_constants.dart';
+import 'package:hotel_manager_app/exception/network_exception.dart';
 import 'package:hotel_manager_app/models/employee.dart';
+import 'package:http/http.dart' as http;
 
-
-class EmployeeDataNetworkController extends GetxController{
+class EmployeeDataNetworkController extends GetxController {
   static EmployeeDataNetworkController instance = Get.find();
 
-  Future<List<Map<String, dynamic>>> getEmployees() async{
-    //TODO:send http request
-    List<Map<String, dynamic>> currentEmployeeList = [];
-    return currentEmployeeList;
+  Future<List<Map<String, dynamic>>> getEmployees() async {
+    try{
+      List<Map<String, dynamic>> currentEmployeeList = [];
+
+      Uri uri = Uri.parse('${NetworkConstants.baseUrl}/employee/all');
+
+      var response = await http.get(uri);
+
+      if(response.statusCode == 200)
+        {
+          try{
+            currentEmployeeList = response as List<Map<String, dynamic>>;
+            return currentEmployeeList;
+          }
+          catch(e){
+            //handle parse error
+          }
+        }
+      else{
+        throw NetworkException(message: 'Network Error due to : Status ${response.statusCode}, ${response.body}');
+      }
+      return currentEmployeeList;
+    } catch(e){
+      print('Error getting/parsing employee data');
+      print(e.toString());
+      throw NetworkException(message: e.toString());
+    }
   }
 
-  Future<Employee> addEmploy({required Employee employee}) async{
-    //TODO:send http with jason employee
-    Map<String,dynamic> employeeMap = employee.toMap();
-    return Employee(name: '', role: 'Chef', id: '001', email: 'email', nic: 'nic', phone_no: 'phone_no');
+  Future<Map<String, dynamic>> addEmploy({required Employee employee}) async {
+    Map<String, dynamic> employeeMap = {};
+    Uri url = Uri.parse('${NetworkConstants.baseUrl}/employee/add');
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var employeeMap = jsonDecode(response.body) as Map<String, dynamic>;
+      print('employee data have');
+      return employeeMap;
+    } else {
+      print(
+          'addEmploy Request failed with status: ${response.statusCode}.');
+      throw NetworkException();
+    }
   }
-
-
 }

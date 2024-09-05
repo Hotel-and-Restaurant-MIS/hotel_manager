71cbcb1aaddd8d84e9d9f43bc0afba5a177306af
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-05 10:23:06 +0530

    original code is commented and, dummy data is getting. have to create backend and connect with the back end.

--- lib/controllers/data/booking_data_controller.dart
+++ lib/controllers/data/booking_data_controller.dart
@@ -41,3 +41,90 @@ class BookingDataController extends GetxController {
     //TODO: complete add booking method.
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:get/get.dart';
+// import 'package:hotel_manager_app/constants/booking_constants.dart';
+// import 'package:hotel_manager_app/controllers/network/booking_data_network_controller.dart';
+// import 'package:hotel_manager_app/models/booking.dart';
+//
+// class BookingDataController extends GetxController {
+//   static BookingDataController instance = Get.find();
+//
+//   final BookingDataNetworkController _bdnc = BookingDataNetworkController.instance;
+//
+//   Map<String, List<Booking>> _bookingDataMap = {};
+//
+//   Map<String, List<Booking>> get bookingDataMap => _bookingDataMap;
+//
+//   BookingDataController._();
+//
+//   static Future<BookingDataController> create() async {
+//     final BookingDataController controller = BookingDataController._();
+//     await controller._initController();
+//     return controller;
+//   }
+//
+//   @override
+//   void onInit() {
+//     // TODO: implement onInit
+//     super.onInit();
+//     _initController();
+//   }
+//
+//   Future<void> _initController() async {
+//     _bookingDataMap = await _getBookingList();
+//   }
+//
+//   Future<List<Booking>> _getBookingList() async {
+//     Map<String, List<Booking>> bookingListMap = {};
+//
+//     try {
+//       Map<String, List<dynamic>> bookingMap = await _bdnc.getBooking();
+//       for (var entry in bookingMap.entries) {
+//         String key = entry.key; //status
+//         List value = entry.value; // bookings
+//
+//         bookingListMap[key] = value
+//             .map<Booking>((singleBooking) => Booking.fromMap(singleBooking))
+//             .toList();
+//       }
+//     } catch (e) {
+//       print(e);
+//       print('error occurs getting booking data');
+//     } finally {
+//       return bookingListMap;
+//     }
+//     return kBookingList;
+//   }
+//
+//   void addBooking({required Booking booking}) {
+//     //TODO: complete add booking method.
+//   }
+// }

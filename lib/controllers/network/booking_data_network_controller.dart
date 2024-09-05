1546d6bf6ee0c3193374c249f88181a8b5d838ca
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-05 10:22:57 +0530

    original code is commented and, dummy data is getting. have to create backend and connect with the back end.

--- lib/controllers/network/booking_data_network_controller.dart
+++ lib/controllers/network/booking_data_network_controller.dart
@@ -32,3 +32,111 @@ class BookingDataNetworkController extends GetxController {
     });
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'dart:convert';
+//
+// import 'package:get/get.dart';
+// import 'package:hotel_manager_app/constants/network_constants.dart';
+// import 'package:hotel_manager_app/exception/list_pass_exception.dart';
+// import 'package:hotel_manager_app/exception/network_exception.dart';
+// import 'package:hotel_manager_app/models/booking.dart';
+// import 'package:http/http.dart' as http;
+//
+// class BookingDataNetworkController extends GetxController {
+//   static BookingDataNetworkController instance = Get.find();
+//
+//   Future<Map<String, List<dynamic>>> getBooking() async {
+//     //TODO: send http request
+//     List<Map<String, dynamic>> bookingList = await getBookingList();
+//     List<Map<String, dynamic>> reservationList = await getReservationList();
+//
+//     Map<String, List<dynamic>> bookingAndReservationMap = {};
+//     bookingAndReservationMap['Ongoing'] = reservationList;
+//     bookingAndReservationMap['Completed'] = reservationList;
+//
+//     return bookingAndReservationMap;
+//   }
+//
+//   Future<List<Map<String, dynamic>>> getReservationList() async {
+//     List<Map<String, dynamic>> reservationList = [];
+//     List<dynamic> decodedList = [];
+//     // Uri url = Uri.http('${NetworkConstants.baseUrl}/reservation/all');
+//     Uri url = Uri.parse('${NetworkConstants.baseUrl}/reservations/all');
+//     var response = await http.get(url);
+//     if (response.statusCode == 200) {
+//       try {
+//         print(response.body);
+//         reservationList =
+//             (jsonDecode(response.body) as List<dynamic>)
+//                 .map<Map<String, dynamic>>(
+//                     (booking) => booking as Map<String, dynamic>)
+//                 .toList();
+//         //reservationList = decodedList.cast<Map<String, dynamic>>();
+//         return reservationList;
+//       } catch (e) {
+//         print(e);
+//         throw ListPassException(
+//             message: 'json decode error in getReservationList');
+//       }
+//     } else {
+//       print(
+//           'get reservation Request failed with status: ${response.statusCode}.');
+//       throw NetworkException();
+//     }
+//   }
+//
+//   Future<List<Map<String, dynamic>>> getBookingList() async {
+//     List<Map<String, dynamic>> bookingList = [];
+//     List<dynamic> decodedList = [];
+//     Uri url = Uri.parse('${NetworkConstants.baseUrl}/reservations/booking/all');
+//     var response = await http.get(url);
+//     if (response.statusCode == 200) {
+//       try {
+//         // jsonDecode(response.body) as List<Map<String, dynamic>>;
+//         bookingList = (jsonDecode(response.body) as List<dynamic>)
+//             .map<Map<String, dynamic>>(
+//                 (booking) => booking as Map<String, dynamic>)
+//             .toList();
+//         // bookingList = decodedList.cast<Map<String, dynamic>>();
+//         return bookingList;
+//       } catch (e) {
+//         print(e);
+//         throw ListPassException(message: 'json decode error in getBookingList');
+//       }
+//     } else {
+//       print('get booking Request failed with status: ${response.statusCode}.');
+//       throw NetworkException();
+//     }
+//   }
+//
+//   Future<Map<String,dynamic>> addBooking({required Booking booking}) async{
+//     Map<String,dynamic> bookingMap ={};
+//     Uri url = Uri.parse('${NetworkConstants.baseUrl}/');
+//
+//   }
+// }

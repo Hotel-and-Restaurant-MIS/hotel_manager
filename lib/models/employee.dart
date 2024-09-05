87e3bee10fdc88599f5c3ad62c6a3f5520adf323
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-05 10:17:18 +0530

    add toMap and fromMap

--- lib/models/employee.dart
+++ lib/models/employee.dart
@@ -6,13 +6,34 @@ class Employee {
   final String nic;
   final String phone_no;
 
+  Employee({
+    required this.name,
+    required this.role,
+    required this.id,
+    required this.email,
+    required this.nic,
+    required this.phone_no,
+  });
 
-  Employee(
-      {required this.name,
-      required this.role,
-       this.id ='0',
-      required this.email,
-      required this.nic,
-      required this.phone_no,
-      });
+  // Method to convert an instance of MenuItem to a map
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'role': role,
+      'id': id,
+      'email': email,
+      'nic': nic,
+      'phone_no': phone_no,
+    };
+  }
+  // Factory constructor to create an instance of MenuItem from a JSON
+  factory Employee.fromMap(Map<String, dynamic> map) {
+    return Employee(
+        id: map['id'],
+        name: map['name'],
+        role: map['role'],
+        email: map['email'],
+        nic: map['nic'],
+        phone_no: map['phone_no']);
+  }
 }

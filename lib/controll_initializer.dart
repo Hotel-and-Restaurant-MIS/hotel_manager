720dc6ca878f9d5b8c049c0edd9ef9752a523463
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-23 15:44:43 +0530

    init controllers that want for the loginScreen

--- lib/controll_initializer.dart
+++ lib/controll_initializer.dart
@@ -2,9 +2,11 @@ import 'package:hotel_manager_app/controllers/data/booking_data_controller.dart'
 import 'package:hotel_manager_app/controllers/data/create_booking_data_controller.dart';
 import 'package:hotel_manager_app/controllers/data/employee_data_controller.dart';
 import 'package:get/get.dart';
+import 'package:hotel_manager_app/controllers/data/login_data_controller.dart';
 import 'package:hotel_manager_app/controllers/network/booking_data_network_controller.dart';
 import 'package:hotel_manager_app/controllers/network/create_booking_network_controller.dart';
 import 'package:hotel_manager_app/controllers/network/employee_data_network_controller.dart';
+import 'package:hotel_manager_app/controllers/network/login_network_controller.dart';
 import 'package:hotel_manager_app/controllers/views/add_employee_screen/add_emp_state_controller.dart';
 import 'package:hotel_manager_app/controllers/views/available_room_screen/available_room_state_controller.dart';
 import 'package:hotel_manager_app/controllers/views/booking_detail_screen/booking_detail_state_controller.dart';
@@ -12,6 +14,7 @@ import 'package:hotel_manager_app/controllers/views/booking_detail_screen/comple
 import 'package:hotel_manager_app/controllers/views/booking_management_screen/booking_list_builder.dart';
 import 'package:hotel_manager_app/controllers/views/create_booking_screen/room_grid_builder.dart';
 import 'package:hotel_manager_app/controllers/views/employee_screen/employee_list_builder.dart';
+import 'package:hotel_manager_app/controllers/views/login_screen/login_state_controller.dart';
 
 import 'controllers/views/create_booking_screen/create_booking_state_controller.dart';
 
@@ -22,6 +25,9 @@ class ControllerInitializer {
 
   static Future<void> initAllControllers() async {
     try {
+      Get.put(LoginNetworkController());
+      Get.put(LoginDataController());
+      Get.put(LoginStateController());
       Get.put(EmployeeDataNetworkController());
       Get.put(CreateBookingNetworkController());
       Get.put(CreateBookingDataController());

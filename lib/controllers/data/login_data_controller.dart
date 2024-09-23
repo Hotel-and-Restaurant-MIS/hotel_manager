a7ffc910912d850342f395db899b2ea8c884899a
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-23 15:42:49 +0530

    login_data_controller.dart is created.

---
+++ lib/controllers/data/login_data_controller.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+import 'package:hotel_manager_app/controllers/network/login_network_controller.dart';
+
+class LoginDataController extends GetxController{
+
+  static LoginDataController instance = Get.find();
+
+  LoginNetworkController _lnc = LoginNetworkController.instance;
+
+  RxBool _isValidUser =false.obs;
+
+  bool get isValidUser => _isValidUser.value;
+
+  void setIsValidUser(bool value) {
+    _isValidUser.value = value;
+  }
+
+  Future<void> CheckUser(String password) async{
+    setIsValidUser(await _lnc.authUser(password));
+  }
+
+}

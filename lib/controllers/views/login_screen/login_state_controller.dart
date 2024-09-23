d0fcb3755ee8ae9b1681ceaa40170ffc53aac1f7
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-23 15:42:16 +0530

    login_state_controller.dart is created

---
+++ lib/controllers/views/login_screen/login_state_controller.dart
@@ -0,0 +1,34 @@
+import 'package:get/get.dart';
+import 'package:hotel_manager_app/controllers/data/login_data_controller.dart';
+
+
+class LoginStateController extends GetxController{
+  static LoginStateController instance = Get.find();
+
+
+  LoginDataController _ldc = LoginDataController.instance;
+
+  RxBool isSubmitted = false.obs;
+  String? _password;
+
+  String? get password => _password;
+
+  void setPassword(String value) {
+    _password = value;
+  }
+
+  String? _userName;
+
+  String? get userName => _userName;
+
+  void setUserName(String value) {
+    _userName = value;
+  }
+
+  Future<void> checkUser()async{
+
+   await _ldc.CheckUser(_password!);
+   isSubmitted.value = true;
+  }
+
+}

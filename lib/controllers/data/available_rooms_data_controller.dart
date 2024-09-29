c844ae1c6b0a030e810f16e8eef3c1d1a96039e1
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-29 15:04:49 +0530

    available_room_network_controller.dart, available_rooms_data_controller.dart are created for get the available room details for the availableScreen.

---
+++ lib/controllers/data/available_rooms_data_controller.dart
@@ -0,0 +1,59 @@
+import 'package:get/get.dart';
+import 'package:hotel_manager_app/controllers/network/available_room_network_controller.dart';
+import 'package:hotel_manager_app/models/roomType.dart';
+
+
+class AvailableRoomsDataController extends GetxController{
+
+  static AvailableRoomsDataController instance = Get.find();
+
+  AvailableRoomsNetworkController _arnc = AvailableRoomsNetworkController.instance;
+
+  RxInt _singleRoomCount = 0.obs;
+  RxInt _doubleRoomCount = 0.obs;
+  RxInt _twinRoomCount = 0.obs;
+
+  int? get singleRoomCount => _singleRoomCount.value;
+  int? get doubleRoomCount => _doubleRoomCount.value;
+  int? get twinRoomCount => _twinRoomCount.value;
+
+  void setSingleRoomCount(int value) {
+    _singleRoomCount.value = value;
+    update();
+  }
+
+  void setDoubleRoomCount(int value) {
+    _doubleRoomCount.value = value;
+    update();
+  }
+
+  void setTwinRoomCount(int value) {
+    _twinRoomCount.value = value;
+    update();
+  }
+
+  Future<void> getAvailableRoomCount({
+    required List<DateTime> dateList,
+  }) async {
+    List<RoomType> roomTypeCountList =[];
+    try{
+      List<Map<String, dynamic>> roomCountMapList = await _arnc.getAvailableRoomCount(dateList: dateList);
+      roomCountMapList.forEach((Map<String, dynamic> roomTypeMap){
+        roomTypeCountList.add(RoomType.fromMap(roomTypeMap));
+      });
+      for(RoomType roomType in roomTypeCountList){
+        if(roomType.roomTypeName == 'SINGLE ROOM'){
+          setSingleRoomCount(roomType.availableCount);
+        }else if(roomType.roomTypeName == 'DOUBLE ROOM'){
+          setDoubleRoomCount(roomType.availableCount);
+        }else if(roomType.roomTypeName == 'TWIN ROOM'){
+          setTwinRoomCount(roomType.availableCount);
+        }
+      }
+      print('single Room: $_singleRoomCount');
+    }catch(e){
+      print('Error occurs getting available room count for all room type');
+      print(e.toString());
+    }
+  }
+}
---
+++ lib/controllers/network/available_room_network_controller.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:get/get.dart';
+import 'package:hotel_manager_app/constants/network_constants.dart';
+import 'package:hotel_manager_app/exception/list_pass_exception.dart';
+import 'package:hotel_manager_app/exception/network_exception.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
+
+class  AvailableRoomsNetworkController extends GetxController{
+  static AvailableRoomsNetworkController instance = Get.find();
+
+  Future<List<Map<String, dynamic>>> getAvailableRoomCount(
+      {required List<DateTime> dateList}) async {
+    try {
+      List<Map<String, dynamic>> currentRoomCountList = [];
+
+      Uri url = Uri.parse(
+          '${NetworkConstants.baseUrl}/reservations/booking/roomType/available?from=${DateFormat('yyyy-MM-dd').format(dateList[0])}&to=${DateFormat('yyyy-MM-dd').format(dateList[1])}');
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        try {
+          var jsonResponse = jsonDecode(response.body) as List;
+          currentRoomCountList = jsonResponse
+              .map((roomType) => roomType as Map<String, dynamic>)
+              .toList();
+          return currentRoomCountList;
+
+        } catch (e) {
+          throw ListPassException(
+              message: 'List Passing error at getting available room count');
+        }
+      } else {
+        throw NetworkException(
+            message:
+            'Network Error due to : Status ${response.statusCode}, ${response.body}');
+      }
+    } catch (e) {
+      print('Error getting/parsing available room count data');
+      print(e.toString());
+      throw NetworkException(message: e.toString());
+    }
+  }
+
+}

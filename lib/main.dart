d60ec3bdd0ad5126f36ccd7f7be8a80f85f0b273
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-10-09 18:22:07 +0530

    remove debug label

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class hotel_manager_app extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
     home: LoadingScreen(),
     );
   }
